5924b22c23357bc432be5fd76ac972b348742d08
Naveen Polasa <naveenpolasa99@gmail.com>
2023-01-21 23:42:26 +0530

    update files

--- 01/app.js
+++ 01/app.js
@@ -1,8 +1,8 @@
 document.addEventListener("click", (e) => {
   const body = document.body;
   const div = document.createElement("div");
-  div.style.left = `${e.clientX + window.scrollX - 20}px`;
   div.classList.add("round");
+  div.style.left = `${e.clientX + window.scrollX - 20}px`;
   div.style.top = `${e.clientY + window.scrollY - 20}px`;
   body.appendChild(div);
   setTimeout(() => {
